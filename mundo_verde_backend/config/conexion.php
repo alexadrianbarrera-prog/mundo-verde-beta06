1c47f3f7d22ec038d0c804c9e48e45793c33c630
AlexBarrera80 <1969alexba@gmail.com>
2026-09-21 12:08:03 -0300

    CambDeConex

--- mundo_verde_backend/config/conexion.php
+++ mundo_verde_backend/config/conexion.php
@@ -11,7 +11,7 @@
 $host = 'localhost';
 $db   = 'u366545585_Vivero80';
 $user = 'u366545585_MundoVerde';
-$pass = 'GreenLife80'; // <-- Cambiar a la contraseña real de la base de datos en Hostinger
+$pass = './TPeEXb7ZJ1v7Wv'; // <-- Cambiar a la contraseña real de la base de datos en Hostinger
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
